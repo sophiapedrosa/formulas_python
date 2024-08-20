e2791719eb4eb2fa58c0b944c86357080ace388e
sophiapedrosa <pedrosapya@gmail.com>
2024-08-19 22:28:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+007 MEDIA
+n1 = int (input("digite a primeira nota: "))
+n2 = int (input("digite a segunda nota: "))
+n3 = int (input("digite a terceira nota: "))
+
+media = (n1 + n2 + n3) / 3
+
+print (f"a média das suas notas é = {media:.2f}")
+
+_____________________________________________________
+006 e 005 - OPERAÇÕES
+numero = int (input("digite um numero: "))
+
+sucessor = numero + 1 
+antecessor = numero - 1
+dobro = numero * 2
+triplo = numero * 3
+raizquadrada = numero ** (0.5)
+
+print ("o sucessor é ", sucessor)
+print ("o antecessor é ", antecessor)
+print ("o dobro é ", dobro)
+print ("o triplo é ", triplo)
+print ("a raiz quadrada é ", raizquadrada)
+
+_______________________________________________________
+008 - METROS EM CM E MILI
+metros = float (input("digite um valor em metros: "))
+
+cm = metros * 100
+mili = metros * 1000
+
+print ("em cm é ", cm)
+print ("em milimetros é ", mili)
+
+_________________________________________________________
+009 TABUADA
+
+num = int(input("Digite um número e veja sua tabuada: "))
+
+print ('{} x {} = {}'.format(num, 1, num*1)) 
+print ('{} x {} = {}'.format(num, 2, num*2)) 
+print ('{} x {} = {}'.format(num, 3, num*3)) 
+print ('{} x {} = {}'.format(num, 4, num*4)) 
+print ('{} x {} = {}'.format(num, 5, num*5)) 
+print ('{} x {} = {}'.format(num, 6, num*6)) 
+print ('{} x {} = {}'.format(num, 7, num*7)) 
+print ('{} x {} = {}'.format(num, 8, num*8)) 
+print ('{} x {} = {}'.format(num, 9, num*9)) 
+print ('{} x {} = {}'.format(num, 10, num*10)) 
+
+OU
+
+num = int(input("Digite um número e veja sua tabuada: "))
+
+print ("-"*20)
+print (f"{num} x 01 = {num*1}")
+print (f"{num} x 02 = {num*2}")
+print (f"{num} x 03 = {num*3}")
+print (f"{num} x 04 = {num*4}")
+print (f"{num} x 05 = {num*5}")
+
+______________________________________________________
+
